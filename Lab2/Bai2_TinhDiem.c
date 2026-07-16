b377a6efa4190b885e9f54cb155c4647dea2860b
thanhnghia0609 <aw097e@gmail.com>
2026-07-16 12:46:51 +0700

    Hoan Thanh Lab 2

--- Lab2/Bai2_TinhDiem.c
+++ Lab2/Bai2_TinhDiem.c
@@ -2,11 +2,15 @@
 int main(){ 
     int diemToan, diemLy, diemHoa;
     float diemTB;
-    printf("Nhap diem Toan, Ly, Hoa: ");
-    scanf("%d %d %d", &diemToan, &diemLy, &diemHoa);
-    printf("Diem Toan: %d\n", diemToan);
-    printf("Diem Ly: %d\n", diemLy);
-    printf("Diem Hoa: %d\n", diemHoa);
+    printf("Nhap diem Toan: ");
+    scanf("%d", &diemToan);
+    printf("Nhap diem Ly: ");
+    scanf("%d", &diemLy);
+    printf("Nhap diem Hoa: ");
+    scanf("%d", &diemHoa);
+
+    
+   
     diemTB = (diemToan*3 + diemLy*2 + diemHoa) / 6.0;
     printf("Diem trung binh: %.2f", diemTB);
     return 0;

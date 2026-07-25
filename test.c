d07cd8b8fb4ab3840264566da42a5b2b3350b56d
thanhnghia0609 <aw097e@gmail.com>
2026-07-25 12:09:53 +0700

    Hoan Thanh Bai Menu

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    int choice ;    
+    do  {
+        printf("+-------------------------------+\n");
+        printf("|    MENU CHUONG TRINH LAB 3    |\n");
+        printf("+-------------------------------+\n");
+        printf("1. Chuc nang tinh hoc luc sinh vien \n");
+        printf("2. Chuc nang giai phuong trinh bac hai \n");
+        printf("3. Chuc nang tinh tien dien tieu thu hang thang \n");
+        printf("4. Thoat chuong trinh \n");
+        printf("Moi chon chuc nang: ");
+        scanf("%d",&choice);
+        switch(choice); {
+            case 1:
+            float diem;
+            printf("")
+            scanf("")
+        }
+}
